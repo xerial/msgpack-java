11800f6cd8e9614e4b41677d6edb358a5d55cbca
Taro L. Saito <leo@xerial.org>
2013-12-14 14:32:02 +0900

    Add build script using sbt

---
+++ core/src/test/scala/org/msgpack/MsgPackSpec.scala
@@ -0,0 +1,38 @@
+package org.msgpack
+
+import org.scalatest._
+import matchers.{MustMatchers, ShouldMatchers}
+import java.io.ByteArrayOutputStream
+import java.lang.String
+import scala.Predef.String
+import scala.language.implicitConversions
+
+trait MesssagePackSpec extends WordSpec with Matchers with GivenWhenThen with OptionValues with Resource with BeforeAndAfter {
+
+  implicit def toTag(t:String) = Tag(t)
+
+  /**
+   * Captures the output stream and returns the printed messages as a String
+   * @param body
+   * @tparam U
+   * @return
+   */
+  def captureOut[U](body: => U) : String = {
+    val out = new ByteArrayOutputStream
+    Console.withOut(out) {
+      body
+    }
+    new String(out.toByteArray)
+  }
+
+  def captureErr[U](body: => U) : String = {
+    val out = new ByteArrayOutputStream
+    Console.withErr(out) {
+      body
+    }
+    new String(out.toByteArray)
+  }
+
+}
+
+
